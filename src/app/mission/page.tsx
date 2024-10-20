2a9d11b976be6a71f0cff6e27174608d21260c03
jeffclaybrook <48254646+jeffclaybrook@users.noreply.github.com>
2024-10-20 17:41:36 -0500

    Add files via upload

---
+++ src/app/mission/page.tsx
@@ -0,0 +1,99 @@
+import Image from "next/image"
+import Header from "@/components/header"
+import Section from "@/components/section"
+
+export default function Mission() {
+ return (
+  <>
+   <Header>Mission</Header>
+   <Section>
+    <Image
+     src="/headshot-2.webp"
+     alt="Craig Claybrook as Thomas Jefferson"
+     width={300}
+     height={100}
+     className="mx-auto rounded-md my-16"
+    />
+    <h2 className="text-2xl font-semibold mb-2">The Mission</h2>
+    <h3 className="mb-8">By Craig L. Claybrook, Reciter of the Declaration</h3>
+    <p className="mb-8 leading-8">For decades and decades, Americans enthusiastically read the <em>Declaration of Independence</em> in the public
+     arena, in schools, and on Fourth of July. We now celebrate our independence but not the document
+     that gave us our independence. It seems to have disappeared into oblivion. So, what can be done
+     to launch the Declaration back onto the national stage? And, why is this so important?</p>
+    <p className="mb-8 leading-8">Consider this reasoning: 1) the significance and impact of the document, 2) the &quot;Foundational
+     Underpinnings&quot; of the document, 3) the deliberate demise of the document&apos;s &quot;worth,&quot; and 4) the
+     overwhelming need for a triumphal return of the document.</p>
+    <p className="mb-8 leading-8"><strong>First, the significance and impact of the document.</strong> Church historians tell us that the Declaration is &quot;one
+     of the two most significant documents in human history, second only to the Bible.&quot; The reason for this is
+     that, not only was it instrumental in ending the tyranny of the king of England, but it was also
+     instrumental in terminating a whopping 2000 years of tyranny in Western Civilization. The magnitude of
+     this achievement is staggering! It obliterated 2000 years of tyranny, dating back to the beginning of the
+     Roman Empire and before the time of Christ! The only feat surpassing the Declaration would be Christ&apos;s
+     redemptive death on the Cross and His offer of forgiveness of sin to mankind!</p>
+    <p className="mb-8 leading-8">The other document happens to be <em>The Constitution of the United States</em>. It took 116 days for the
+     Founding Fathers to draft the Constitution in 1787 but the countries that were set free by the
+     Declaration were still foundering in a governance quandary. In an act of magnanimity, the Founders sent
+     copies of the document to all of those countries. The recipients thought the Constitution was magnificent so, they
+     all became &quot;Constitutional Republics,&quot; as did all fifty of our state governments! Scholars tell us that
+     these two documents stand out as &quot;the pinnacle of Western Civilization since the time of Christ!&quot; Today,
+     the Constitution is under merciless attack by secularists who defy our glorious and God-given
+     beginnings.</p>
+    <p className="mb-8 leading-8"><strong>Second, the &quot;Foundational Underpinnings&quot; of the document.</strong> Secular historians tell us that the pathway
+     to the <em>Declaration of Independence</em> began when the British attacked Lexington, Massachusetts &quot;at dawn
+     on April 19, 1775.&quot; But church historians inform us that the pathway started 700 years prior to that!
+     They also let us know that three pivotal events profoundly shaped America&apos;s Foundational
+     Underpinnings: 1) In 1075, Pope Gregory VII admonished the kings and rulers of Western Europe to stay
+     out of the church&apos;s business. 2) In 1215, Cardinal Stephen Langton introduced his spectacular document,
+     the <em>Magna Carta</em> to &quot;reign in&quot; political tyrants. 3) In 1517, Luther tacked his ninety-five theses onto the
+     Wittenburg door, then inadvertently created two reformations, one religious and one political. Both
+     continue to this day. Politically, he became known for his &quot;Resistance Theory: disobey unjust rulers
+     and laws.&quot;</p>
+    <p className="mb-8 leading-8">Why were these events so vitally important? Pope Gregory VII started the march to America&apos;s freedom
+     with what became known as &quot;freedom of religion.&quot; It also became the Founder&apos;s driving motivation. It also set forth the original meaning of &quot;separation
+     of church and state.&quot; Cardinal Stephen Langton&apos;s 3500-word <em>Magna Carta</em> was designated as Britain&apos;s
+     foremost legal document and it still holds that distinction. It dealt with the foremost legal issue of the
+     Middle Ages, &quot;limiting the power of kings.&quot; The Founders used it extensively. Martin Luther&apos;s theses caused the pope to put him on trial
+     for heresy. When he argued that &quot;it was wrong for anyone to act against his conscience in religious
+     matters,&quot; he set off a firestorm that became known as &quot;liberty of conscience.&quot; The Founders used this as their rallying cry. These three &quot;episodes&quot; in
+     our nation&apos; Foundational Underpinnings paved the way for the Declaration.</p>
+    <p className="mb-8 leading-8"><strong>Third, the deliberate demise of the document&apos;s &quot;worth.&quot;</strong> The demise of the Declaration started out,
+     oddly enough, in a court of law. The 1947 U.S. Supreme Court was considering a case known as <em>Everson
+      v. Board of Education</em>, which had to do with taxpayer dollars being used to transport children to
+     parochial schools. An <em>amicus</em> brief from an atheistic organization introduced the Court to &quot;wall of
+     separation of church and state&quot; with a twist. Pope Gregory VII&apos;s original &quot;separation&quot; in 1075 was meant
+     &quot;to keep the state out of the church&apos;s business&quot; but this had never been codified into law. In order to
+     &quot;secularize America,&quot; the Court then revamped it to mean the exact opposite of its original meaning. (All
+     of this took place during an era when roughly seventy percent of the population attended church.)</p>
+    <p className="mb-8 leading-8">This unprincipled decision lay dormant until the 1963 Supreme Court dredged it up when considering <em>Murray v. Curlett</em>, a case filed by atheist Madalyn Murray O&apos;Hair to ban Bible reading in public schools.
+     Prayer had already been banned in 1962 but this case became the spectacle that launched America&apos;s
+     &quot;secularization/de-Christianization&quot; onto the national stage. This resulted in an onslaught of court cases
+     in the &apos;60s and beyond. What were the results? In short, a multitude of younger folk took the plunge
+     into illegal drugs and &quot;free love.&quot; The decade ended with a tribute to this &quot;de-Christianization&quot; called
+     Woodstock and, by decade&apos;s end, America&apos;s educational system had gone from being &quot;the envy of the
+     world&quot; to utter chaos: plummeting classroom discipline and skyrocketing teen pregnancies.</p>
+    <p className="mb-8 leading-8">How does the Declaration fit into this? Quite simply, the Declaration contains four references to God.
+     These four references are found in the first two sentences and last two sentences, effectively forming
+     bookends around the Declaration. This was the Founders&apos; way of &quot;shouting from the rooftops&quot; that God
+     is &quot;in&quot; the Declaration and &quot;in&quot; the founding of this country! But in recent years, secularists took it upon
+     themselves to deliver the &quot;crowning blow&quot; to the <em>Declaration of Independence</em> and America&apos;s
+     Foundational Underpinnings by eliminating History from the curriculum of most public schools—the
+     ultimate &quot;demise!&quot; Another noteworthy detail is that the Chief Justice who ramrodded &quot;separation&quot; in
+     1947 received the award for &quot;The Most Influential Supreme Court Justice of the Twentieth Century!&quot;</p>
+    <p className="mb-8 leading-8"><strong>Fourth, the overwhelming need for a triumphal return of the document.</strong> These four references to God
+     provide the key elements to &quot;launching the Declaration onto the national stage.&quot; They stand in sharp
+     contrast to the Court&apos;s 1947 definition of &quot;separation of church and state.&quot; But how many people today
+     even know about this contradiction? ... or about Pope Gregory VII&apos;s original &quot;separation of church and
+     state?&quot; ... or about those three pivotal events? ... or about America&apos;s Foundational Underpinnings?
+     Or the fact that these four references are &quot;the most consequential components of the Declaration!&quot;</p>
+    <p className="mb-8 leading-8">As for the &quot;demise of worth,&quot; a plethora of people and organizations can see the need for restoring the Declaration back to its rightful place of authority over our land. We are an army in need of a vision so, focus on: 1) grasping the magnitude of &quot;the
+     two most significant documents in human history, second only to the Bible,&quot; and &quot;the pinnacle of
+     Western Civilization since the time of Christ,&quot; 2) recognizing that the hand of God is still &quot;on&quot; the
+     Declaration, 3) comprehending that, without these four references to God, the Declaration would have
+     been &quot;an empty shell of a document,&quot; and 4) joining forces with others. All of these things are key!</p>
+    <p className="mb-8 leading-8">These four references to God serve as a catalyst for &quot;launching the Declaration onto the national stage.&quot;
+     They also serve as a catalyst for spiritual awakening. They go &quot;hand in hand.&quot; Thank you for your help in &quot;launching&quot; the Declaration with your prayers, concern, and effort!</p>
+    <p className="mb-8 leading-8"><em>Suggested reading: Never Before in History—America&apos;s Inspired Birth, published by Discovery Institute, Seattle WA</em></p>
+   </Section>
+  </>
+ )
+}
